8a0e7a248c6386d820bfb32d4b991b102fdf0506
Esdras <esdras12thiago@gmail.com>
2022-08-24 17:36:20 -0300

    Concluindo requisito 5 e arrumando erros de lint

--- src/App.js
+++ src/App.js
@@ -14,31 +14,67 @@ class App extends React.Component {
       cardImage: '',
       cardRare: '',
       cardTrunfo: false,
-      hasTrunfo: false,
-      isSaveButtonDisabled: false,
-      onInputChange: () => { },
-      onSaveButtonClick: () => { },
+      // hasTrunfo: false,
+      isSaveButtonDisabled: true,
     };
   }
 
   onInputChange = ({ target }) => {
-    const { value, name } = target;
+    const { name, type } = target;
+    const value = type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
-    });
+    }, () => this.buttonChecker());
   };
 
-  onSaveButtonClick = ({ target }) => {
-    const { checked, name } = target;
-    this.setState({
-      [name]: checked,
-    });
+  buttonChecker = () => {
+    const { cardName,
+      cardDescription,
+      cardImage,
+      cardRare,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3 } = this.state;
+    const maxNumber = 210;
+    const maxAttr = 90;
+    const minAttr = 0;
+    const attr1 = +cardAttr1 <= maxAttr && +cardAttr1 >= minAttr;
+    const attr2 = +cardAttr2 <= maxAttr && +cardAttr2 >= minAttr;
+    const attr3 = +cardAttr3 <= maxAttr && +cardAttr3 >= minAttr;
+    console.log(attr1);
+    const allValues = +cardAttr1 + +cardAttr2 + +cardAttr3 <= maxNumber;
+    console.log(allValues);
+    if (
+      cardName
+      && cardDescription
+      && cardImage
+      && cardRare
+      && allValues
+      && attr1
+      && attr2
+      && attr3) {
+      this.setState({
+        isSaveButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        isSaveButtonDisabled: true,
+      });
+    }
   };
 
   render() {
-    const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3 } = this.state;
-    const { cardImage, cardRare, cardTrunfo, hasTrunfo } = this.state;
-    // const { isSaveButtonDisabled} = this.state;
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      isSaveButtonDisabled,
+    } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
@@ -53,6 +89,7 @@ class App extends React.Component {
           cardImage={ cardImage }
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
         />
         <Card
           // onInputChange={ this.onInputChange }
